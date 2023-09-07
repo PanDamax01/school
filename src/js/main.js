4dc0f9a76fe49d2efab897653871f5f7a5879a42
Karol <pandamax.karol.lichon@gmail.com>
2023-09-08 00:27:14 +0200

    repair nav and added sections news and featured

--- src/js/main.js
+++ src/js/main.js
@@ -1,65 +1,51 @@
 const changeNav = () => {
-	;[document.querySelector('.line1'), document.querySelector('.line3')].forEach(
-		(el) => el.classList.toggle('draw-line')
-	)
+	;[document.querySelector('.line1'), document.querySelector('.line3')].forEach(el => el.classList.toggle('draw-line'))
 	document.querySelector('.line2').classList.toggle('submitted-line')
 
 	document.querySelector('.nav').classList.toggle('nav-active')
+	document.body.classList.toggle('hidden')
 }
 
 if (document.body.classList.contains('main-body')) {
-	document
-		.querySelector('.headerTop__btn-nav')
-		.addEventListener('click', changeNav)
+	document.querySelector('.headerTop__btn-nav').addEventListener('click', changeNav)
 }
 
 /////////////////////////////////////////
-
-// let slideNumber = 1
-// const dodaj = () => {
-// 	slideNumber++
-
-//     if (slideNumber === 4) {
-//         slideNumber = 1
-//     }
-
-// 	document.querySelector(
-// 		'.header'
-// 	).style.backgroundImage = `url(dist/img/header${slideNumber}-big.jpg)`
-// }
-
-// const odejmij = () => {
-//     slideNumber--
-
-//     if (slideNumber === 0) {
-//         slideNumber = 3
-//     }
-
-// 	document.querySelector(
-// 		'.header'
-// 	).style.backgroundImage = `url(dist/img/header${slideNumber}-big.jpg)`
-// }
-
-
-// document.querySelector('.header__btn').addEventListener('click', odejmij)
-// document.querySelector('.header__btn--right').addEventListener('click', dodaj)
-
 let slide = 1
 
+setInterval(() => {
+	changeNextSlide()
+}, 8000);
+
 const changeNextSlide = () => {
-    slide === 3 ? slide = 1 : slide++
-    setImg(slide)
+	slide === 3 ? (slide = 1) : slide++
+	setImg(slide)
 }
 
 const changePreSlide = () => {
-    slide === 1 ? slide = 3 : slide--
-    setImg(slide)
+	slide === 1 ? (slide = 3) : slide--
+	setImg(slide)
+}
+
+const changeHeaderText = (slide) => {
+	const headlineHeader = document.querySelector('.header__headline')
+
+	switch (slide) {
+		case 1:
+			headlineHeader.textContent = 'Zmień swoje marzenia w rzeczywistość'
+			break;
+		case 2:
+			headlineHeader.textContent = 'Twój sukces jest naszą misją'
+			break;
+		case 3:
+			headlineHeader.textContent = 'Zapraszamy do szkoły'
+			break;
+	}
 }
 
 const setImg = slide => {
-    document.querySelector(
-        		'.header'
-        	).style.backgroundImage = `url(dist/img/header${slide}-big.jpg)`
+	document.querySelector('.header').style.backgroundImage = `url(dist/img/header${slide}-big.jpg)`
+	changeHeaderText(slide)
 }
 
 document.querySelector('.header__btn').addEventListener('click', changePreSlide)
