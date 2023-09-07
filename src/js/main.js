384159111da983263999d750537a312379970b7a
Karol <pandamax.karol.lichon@gmail.com>
2023-09-07 07:34:51 +0200

    added nav and header

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,66 @@
-document.querySelector('.headerTop__btn-nav').addEventListener('click', ()=>{
-    //zamienic funkce na zmienne do css jako klasa
-    document.querySelector('.line1').style.strokeDasharray = '100, 200'
-    document.querySelector('.line1').style.strokeDashoffset = -121;
+const changeNav = () => {
+	;[document.querySelector('.line1'), document.querySelector('.line3')].forEach(
+		(el) => el.classList.toggle('draw-line')
+	)
+	document.querySelector('.line2').classList.toggle('submitted-line')
 
-    document.querySelector('.line3').style.strokeDasharray = '100, 200'
-    document.querySelector('.line3').style.strokeDashoffset = -121;
+	document.querySelector('.nav').classList.toggle('nav-active')
+}
 
-    document.querySelector('.line2').style.strokeDashoffset = 57;
-})
+if (document.body.classList.contains('main-body')) {
+	document
+		.querySelector('.headerTop__btn-nav')
+		.addEventListener('click', changeNav)
+}
+
+/////////////////////////////////////////
+
+// let slideNumber = 1
+// const dodaj = () => {
+// 	slideNumber++
+
+//     if (slideNumber === 4) {
+//         slideNumber = 1
+//     }
+
+// 	document.querySelector(
+// 		'.header'
+// 	).style.backgroundImage = `url(dist/img/header${slideNumber}-big.jpg)`
+// }
+
+// const odejmij = () => {
+//     slideNumber--
+
+//     if (slideNumber === 0) {
+//         slideNumber = 3
+//     }
+
+// 	document.querySelector(
+// 		'.header'
+// 	).style.backgroundImage = `url(dist/img/header${slideNumber}-big.jpg)`
+// }
+
+
+// document.querySelector('.header__btn').addEventListener('click', odejmij)
+// document.querySelector('.header__btn--right').addEventListener('click', dodaj)
+
+let slide = 1
+
+const changeNextSlide = () => {
+    slide === 3 ? slide = 1 : slide++
+    setImg(slide)
+}
+
+const changePreSlide = () => {
+    slide === 1 ? slide = 3 : slide--
+    setImg(slide)
+}
+
+const setImg = slide => {
+    document.querySelector(
+        		'.header'
+        	).style.backgroundImage = `url(dist/img/header${slide}-big.jpg)`
+}
+
+document.querySelector('.header__btn').addEventListener('click', changePreSlide)
+document.querySelector('.header__btn--right').addEventListener('click', changeNextSlide)
