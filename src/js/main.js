923b3f55602841538a49d4a4a1e8606acddaa581
Karol <pandamax.karol.lichon@gmail.com>
2023-09-08 16:48:37 +0200

    added contact, adds info

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,11 @@
+let slide = 1
+const now = new Date()
+
+document.body.offsetWidth > 350
+	? (document.querySelector('.contact__text--email').textContent = 'support@sparklewpthemes.com')
+	: null
+document.querySelector('.footer__year').textContent = now.getFullYear()
+
 const changeNav = () => {
 	;[document.querySelector('.line1'), document.querySelector('.line3')].forEach(el => el.classList.toggle('draw-line'))
 	document.querySelector('.line2').classList.toggle('submitted-line')
@@ -5,17 +13,6 @@ const changeNav = () => {
 	document.querySelector('.nav').classList.toggle('nav-active')
 	document.body.classList.toggle('hidden')
 }
-
-if (document.body.classList.contains('main-body')) {
-	document.querySelector('.headerTop__btn-nav').addEventListener('click', changeNav)
-}
-
-/////////////////////////////////////////
-let slide = 1
-
-setInterval(() => {
-	changeNextSlide()
-}, 8000);
 
 const changeNextSlide = () => {
 	slide === 3 ? (slide = 1) : slide++
@@ -27,19 +24,19 @@ const changePreSlide = () => {
 	setImg(slide)
 }
 
-const changeHeaderText = (slide) => {
+const changeHeaderText = slide => {
 	const headlineHeader = document.querySelector('.header__headline')
 
 	switch (slide) {
 		case 1:
 			headlineHeader.textContent = 'Zmień swoje marzenia w rzeczywistość'
-			break;
+			break
 		case 2:
 			headlineHeader.textContent = 'Twój sukces jest naszą misją'
-			break;
+			break
 		case 3:
 			headlineHeader.textContent = 'Zapraszamy do szkoły'
-			break;
+			break
 	}
 }
 
@@ -48,5 +45,10 @@ const setImg = slide => {
 	changeHeaderText(slide)
 }
 
+setInterval(() => {
+	changeNextSlide()
+}, 8000)
+
 document.querySelector('.header__btn').addEventListener('click', changePreSlide)
 document.querySelector('.header__btn--right').addEventListener('click', changeNextSlide)
+document.querySelector('.headerTop__btn-nav').addEventListener('click', changeNav)
