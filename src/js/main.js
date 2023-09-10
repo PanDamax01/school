ded8bda4e803cd687e717ffc55bc7890f13c3a70
Karol <pandamax.karol.lichon@gmail.com>
2023-09-11 00:23:10 +0200

    added new js

--- src/js/main.js
+++ src/js/main.js
@@ -1,54 +1,110 @@
-let slide = 1
-const now = new Date()
+let spanYear, contactEmail, burgerLines, burgerLine2, burgerNav, nav, body, headlineHeader, header, headerBtnLeft , headerBtnRight, disabledIcon, asideDisabled, navItem, disabledItem, rootStyles
 
-document.body.offsetWidth > 350
-	? (document.querySelector('.contact__text--email').textContent = 'support@sparklewpthemes.com')
-	: null
-document.querySelector('.footer__year').textContent = now.getFullYear()
+let slide = 1, isClick = false, timeNextSlide = 9_000, fontSize = 2
+const motivationText = ['Zmień swoje marzenia w rzeczywistość', 'Twój sukces jest naszą misją', 'Zapraszamy do szkoły']
+
+
+const main = () => {
+	prepareDOMElements()
+	onStart()
+	prepareDOMEvents()
+}
+
+const prepareDOMElements = () => {
+	body = document.body
+  	spanYear = body.querySelector('.footer__year')
+  	contactEmail = body.querySelector('.contact__text--email')
+  	burgerLines = [body.querySelector('.line1'), body.querySelector('.line3')]
+  	burgerLine2 = body.querySelector('.line2')
+  	burgerNav = body.querySelector('.headerTop__btn-nav')
+  	nav = body.querySelector('.nav')
+	  navItem = body.querySelectorAll('.nav__item')
+  	headlineHeader = body.querySelector('.header__headline')
+  	header = body.querySelector('.header')
+  	headerBtnLeft = body.querySelector('.header__btn')
+  	headerBtnRight = body.querySelector('.header__btn--right')
+	disabledIcon = body.querySelector('.accessible-tools__img')
+	asideDisabled = body.querySelector('.accessible-tools')
+	disabledItem = body.querySelectorAll('.accessible-tools__item')
+	// rootStyles = getComputedStyle(document.documentElement)
+	// let size = rootStyles.getPropertyValue('--kolor-tekstu')
+}
+
+//zamienic na osobne funkcje
+const onStart = () => {
+	const year = new Date().getFullYear()
+	spanYear.textContent = year
+
+	//true false
+	body.offsetWidth > 350?(contactEmail.textContent = 'support@sparklewpthemes.com'):null
+}
 
 const changeNav = () => {
-	;[document.querySelector('.line1'), document.querySelector('.line3')].forEach(el => el.classList.toggle('draw-line'))
-	document.querySelector('.line2').classList.toggle('submitted-line')
-
-	document.querySelector('.nav').classList.toggle('nav-active')
-	document.body.classList.toggle('hidden')
-}
-
-const changeNextSlide = () => {
-	slide === 3 ? (slide = 1) : slide++
-	setImg(slide)
-}
-
-const changePreSlide = () => {
-	slide === 1 ? (slide = 3) : slide--
-	setImg(slide)
-}
-
-const changeHeaderText = slide => {
-	const headlineHeader = document.querySelector('.header__headline')
-
-	switch (slide) {
-		case 1:
-			headlineHeader.textContent = 'Zmień swoje marzenia w rzeczywistość'
-			break
-		case 2:
-			headlineHeader.textContent = 'Twój sukces jest naszą misją'
-			break
-		case 3:
-			headlineHeader.textContent = 'Zapraszamy do szkoły'
-			break
+	if (body.offsetWidth <= 767) {
+		burgerLines.forEach(el => el.classList.toggle('draw-line'))
+		burgerLine2.classList.toggle('submitted-line')
+	
+		nav.classList.toggle('active')
+		body.classList.toggle('hidden')
 	}
 }
 
-const setImg = slide => {
-	document.querySelector('.header').style.backgroundImage = `url(dist/img/header${slide}-big.jpg)`
+const changeNumber = e => {
+	slide = e.target === headerBtnRight ? (slide % 3) + 1 : (slide - 2 + 3) % 3 + 1
+  	setImg(slide)
 	changeHeaderText(slide)
+	isClick = true
 }
 
-setInterval(() => {
-	changeNextSlide()
-}, 8000)
+const setImg = slide => {
+	const size = body.offsetWidth < 576 ? 'small': 'big'	
+	header.style.backgroundImage = `url(dist/img/header${slide}-${size}.jpg)`
+}
 
-document.querySelector('.header__btn').addEventListener('click', changePreSlide)
-document.querySelector('.header__btn--right').addEventListener('click', changeNextSlide)
-document.querySelector('.headerTop__btn-nav').addEventListener('click', changeNav)
+const changeHeaderText = slide => {
+	headlineHeader.textContent = motivationText[slide -1];
+}
+
+const autoChangeSlide = () => {
+	if (!isClick) {
+		slide === 3 ? slide = 1 : slide ++
+		setImg(slide)
+		changeHeaderText(slide)
+	}else {
+		isClick = false
+	}
+}
+
+const changeAsideDisabled = () => {
+	asideDisabled.classList.toggle('active')
+}
+
+const changeValueSpecial = e => {
+	let attribute = e.target.closest('li').getAttribute('data-tool')
+
+	if (attribute === 'increase-text') {
+		fontSizeUp()
+	}
+}
+
+const fontSizeUp = () =>{
+	const currentFontSize = window.getComputedStyle(body).getPropertyValue('font-size')
+	let fontSize = parseFloat(currentFontSize);
+	console.log(currentFontSize);
+	fontSize += 0.2
+	body.style.fontSize = fontSize + 'px'
+}
+
+const prepareDOMEvents = () => {
+	burgerNav.addEventListener('click', changeNav)
+	navItem.forEach(li => li.addEventListener('click', changeNav))
+	headerBtnRight.addEventListener('click', changeNumber)
+	headerBtnLeft.addEventListener('click', changeNumber)
+	disabledIcon.addEventListener('click', changeAsideDisabled)
+	disabledItem.forEach(li => li.addEventListener('click', changeValueSpecial))
+	
+
+	setInterval(autoChangeSlide, timeNextSlide)
+}
+
+document.addEventListener('DOMContentLoaded', main)
